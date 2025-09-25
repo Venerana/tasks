084a291c1163935a6f2d3fabf142a1b0759e59d6
venerana <davidvenerana@hotmail.com>
2025-09-25 08:21:18 -0300

    Adicionar configuração inicial e endpoint de ping

--- app/config.py
+++ app/config.py
@@ -1,4 +1,13 @@
 import logging
-
-from pydantic_settings  import BaseSettings
-
+ 
+from pydantic_settings import BaseSettings
+ 
+log = logging.getLogger("uvicorn")
+ 
+class Settings(BaseSettings):
+    environment: str = "dev"
+    testing: bool = bool(0)
+ 
+def get_settings() -> Settings:
+    log.info("Loading settings")
+    return Settings()
